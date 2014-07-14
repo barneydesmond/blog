c65625ab1f879196e69613f7b545f4df6e6bd591
Barney Desmond <barneydesmond@gmail.com>
2014-07-14 14:11:18 +1000

    Harmonise plugin parameter names with the usual
    Former-commit-id: 0e13a94bcd3d7134fb61ea61950a69040ef2d272

--- _plugins/insert_image.rb
+++ _plugins/insert_image.rb
@@ -1,16 +1,16 @@
 module Jekyll
   class InsertImageTag < Liquid::Tag
 
-    def initialize(tag_name, image_name, tokens)
+    def initialize(tag_name, text, tokens)
       super
-      @image_name = image_name.strip
+      @text = text.strip
     end
 
     def render(context)
       img_full_path  = context.registers[:site].config['img_full_path']
       img_thumb_path = context.registers[:site].config['img_thumb_path']
-      #"<p><a href=\"#{img_full_path}/#{@image_name}\"><img alt=\"ALT TEXT GOES HERE\" title=\"TITLE TEXT GOES HERE\" src=\"#{img_thumb_path}/#{@image_name}\"/></a></p>"
-      "<p class=\"inline-embed\"><a href=\"#{img_full_path}/#{@image_name}\"><img alt=\"alt-text placeholder for #{@image_name}\" src=\"#{img_thumb_path}/#{@image_name}\" /></a></p>"
+      #"<p><a href=\"#{img_full_path}/#{@text}\"><img alt=\"ALT TEXT GOES HERE\" title=\"TITLE TEXT GOES HERE\" src=\"#{img_thumb_path}/#{@text}\"/></a></p>"
+      "<p class=\"inline-embed\"><a href=\"#{img_full_path}/#{@text}\"><img alt=\"alt-text placeholder for #{@text}\" src=\"#{img_thumb_path}/#{@text}\" /></a></p>"
     end
   end
 end
